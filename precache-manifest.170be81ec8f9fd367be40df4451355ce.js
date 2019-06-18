2f0baaf7b5f18c1128c1694fbfd6d05490283ce1
Boxing199 <virkolomeo@gmail.com>
2019-06-18 23:54:41 +0300

    Updates

--- precache-manifest.170be81ec8f9fd367be40df4451355ce.js
+++ precache-manifest.170be81ec8f9fd367be40df4451355ce.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "78a59ecfba685d55b289e8613dc6763c",
+    "revision": "5f1490a32eaa5314d4ad169493268863",
     "url": "/react-first/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-first/static/css/2.8b967ecf.chunk.css"
   },
   {
-    "revision": "97757bd48b053f4f2c36",
-    "url": "/react-first/static/css/main.5a2337c5.chunk.css"
+    "revision": "4f92f120644655515f24",
+    "url": "/react-first/static/css/main.bdf1ff7b.chunk.css"
   },
   {
     "revision": "09d3f0f38e84b6848668",
     "url": "/react-first/static/js/2.c25611e8.chunk.js"
   },
   {
-    "revision": "97757bd48b053f4f2c36",
-    "url": "/react-first/static/js/main.2bf7299f.chunk.js"
+    "revision": "4f92f120644655515f24",
+    "url": "/react-first/static/js/main.c6f35036.chunk.js"
   },
   {
     "revision": "4b0a6ef6f7219535661b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-first/precache-manifest.b6e52d1ba7fbecdab1c27e5e34329b3a.js"
+  "/react-first/precache-manifest.170be81ec8f9fd367be40df4451355ce.js"
 );
 
 self.addEventListener('message', (event) => {
